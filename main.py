f19afe7ec9a54a206573852aea66e039bd64f2ca
Arik <kenan.arik@intel.com>
2019-12-16 16:48:34 -0800

    moved html to file

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 from starlette.websockets import WebSocket
+from starlette.responses import FileResponse
+import random
 
+# run this command: uvicorn main:app --reload
 
 app = FastAPI()
 
-app.mount("./", StaticFiles(directory))
+
 
 @app.get("/")
 async def get():
-    return HTMLResponse(html)
-
+    return FileResponse('static/index.html')
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
+        print(data)
+        data = random.random()
         await websocket.send_text(f"Message text was: {data}")
